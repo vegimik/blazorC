a459484ee1130de01d0caa5efbd8327e4af9d35f
Vegim Karaçica <vegimi.k@hotmail.com>
2022-07-29 17:05:14 +0200

    Blazor Course - Full-Stack C# Web Apps

---
+++ BlazorAppServerSide/BlazorAppServerSide/BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorAppServerSide.BaseComponents
+{
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime dateTime { get; set; }
+    }
+}
